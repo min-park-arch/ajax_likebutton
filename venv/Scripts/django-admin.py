4ff1da0a79f848acc1d5fafb6481816b9a3d32fe
min <0707369@yonsei.ac.kr>
2021-08-15 22:03:32 +0900

    좋아요 버튼 1차 구현 완료

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,21 @@
+#!c:\users\min\onedrive - 연세대학교 (yonsei university)\0_2021\2021_newteam_멋사\개인추가공부\ajax_likebutton\venv\scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
